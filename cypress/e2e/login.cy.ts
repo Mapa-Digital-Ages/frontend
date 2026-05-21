cdf061572906261e8eb8217fd75ad361e2a6f521
GuilhermeGMV <gui141202@gmail.com>
2026-05-20 21:46:03 -0300

    test: enhance login test assertions for ALUNO role

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,5 +5,10 @@ describe('Página de Login - Fluxos do Front-end', () => {
 
   it('deve logar com sucesso como ALUNO', () => {
     cy.login('aluno')
+    cy.url().should('include', '/student/')
+    cy.url().should('not.include', '/admin')
+    cy.url().should('not.include', '/parent')
+    cy.url().should('not.include', '/school')
+    cy.url().should('not.include', '/company')
   })
 })
