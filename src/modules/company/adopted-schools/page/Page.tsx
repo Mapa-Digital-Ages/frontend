776ff0cdcca81412e13225fc491f42ba0cd2d5db
GuilhermeGMV <gui141202@gmail.com>
2026-05-27 11:18:45 -0300

    feat: implement adopted schools page and update routing

---
+++ src/modules/company/adopted-schools/page/Page.tsx
@@ -0,0 +1,16 @@
+import AppPageContainer from '@/shared/ui/AppPageContainer'
+import PageHeader from '@/shared/ui/PageHeader'
+
+function AdoptedSchoolsPage() {
+  return (
+    <AppPageContainer>
+      <PageHeader
+        title="Escolas Apadrinhadas"
+        subtitle="Gerencie as escolas apadrinhadas pela sua empresa"
+        variant="company"
+      />
+    </AppPageContainer>
+  )
+}
+
+export default AdoptedSchoolsPage
